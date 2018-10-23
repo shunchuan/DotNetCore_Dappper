00ea69f0455c2de7f8436d7d7edc85efa285b2b4
shunchuan <1289158751@qq.com>
2018-10-23 10:35:15 +0800

    feat:中间件Sample

---
+++ DotNetCore_Dappper.Infrastructure/Middleware/ConfigurationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetCore_Dappper.Infrastructure.Middleware
+{
+    public static class ConfigurationExtensions
+    {
+        /// <summary>
+        /// 注册中间件
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder AppUseMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<SampleMiddleware>();
+        }
+    }
+}
---
+++ DotNetCore_Dappper.Infrastructure/Middleware/SampleMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCore_Dappper.Infrastructure.Middleware
+{
+    public class SampleMiddleware
+    {
+        private readonly RequestDelegate _next;
+         
+         public SampleMiddleware(RequestDelegate next)
+         {
+             _next = next;
+         }
+ 
+         public async Task Invoke(HttpContext context)
+         {
+             PreProceed(context);
+             await _next(context);
+             PostProceed(context);
+         }
+ 
+         private void PreProceed(HttpContext context)
+         {
+             Console.WriteLine($"{DateTime.Now} sample middleware invoke preproceed");
+         }
+ 
+         private void PostProceed(HttpContext context)
+         {
+             Console.WriteLine($"{DateTime.Now} sample middleware invoke postproceed");
+         }
+    }
+}
